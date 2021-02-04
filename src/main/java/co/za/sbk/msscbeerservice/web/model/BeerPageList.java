681c7e8923018e493b560a39010406b7277514ca
Thato Seboko <thato.seboko@gmail.com>
2021-02-04 11:45:57 +0200

    Added Beer Data Model. Closes #2

---
+++ src/main/java/co/za/sbk/msscbeerservice/web/model/BeerPageList.java
@@ -0,0 +1,20 @@
+package co.za.sbk.msscbeerservice.web.model;
+
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+/**
+ * @author Thato Seboko on 2021/02/04
+ */
+public class BeerPageList extends PageImpl<BeerDto> {
+
+    public BeerPageList(List<BeerDto> content, Pageable pageable, long total) {
+        super(content, pageable, total);
+    }
+
+    public BeerPageList(List<BeerDto> content) {
+        super(content);
+    }
+}
---
+++ src/main/java/co/za/sbk/msscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,8 @@
+package co.za.sbk.msscbeerservice.web.model;
+
+/**
+ * @author Thato Seboko on 2021/02/04
+ */
+public enum BeerStyleEnum {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
