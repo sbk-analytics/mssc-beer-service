eeed9335ebf35be2174e64e6d7273995086c068b
Thato Seboko <thato.seboko@gmail.com>
2021-02-04 19:52:09 +0200

    Initialize data using sql

--- src/main/java/co/za/sbk/msscbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/co/za/sbk/msscbeerservice/bootstrap/BeerLoader.java
@@ -14,7 +14,7 @@ import java.math.BigDecimal;
  * @author Thato Seboko on 2021/02/04
  */
 @RequiredArgsConstructor
-@Component
+//@Component
 public class BeerLoader implements CommandLineRunner {
 
     public static final String BEER_1_UPC = "0631234200036";
