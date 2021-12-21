e10aeec785187b2f5f8dd600963d0c6ad994e8ec
Thato Seboko <thato.seboko@gmail.com>
2021-12-21 22:57:55 +0200

    Added jason configuration for list

--- src/main/java/co/za/sbk/msscbeerservice/web/model/BeerPagedList.java
+++ src/main/java/co/za/sbk/msscbeerservice/web/model/BeerPagedList.java
@@ -14,20 +14,20 @@ public class BeerPagedList extends PageImpl<BeerDto> implements Serializable {
 
     static final long serialVersionUID = 1114715135625836949L;
 
-//    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
-//    public BeerPagedList(@JsonProperty("content") List<BeerDto> content,
-//                         @JsonProperty("number") int number,
-//                         @JsonProperty("size") int size,
-//                         @JsonProperty("totalElements") Long totalElements,
-//                         @JsonProperty("pageable") JsonNode pageable,
-//                         @JsonProperty("last") boolean last,
-//                         @JsonProperty("totalPages") int totalPages,
-//                         @JsonProperty("sort") JsonNode sort,
-//                         @JsonProperty("first") boolean first,
-//                         @JsonProperty("numberOfElements") int numberOfElements) {
-//
-//        super(content, PageRequest.of(number, size), totalElements);
-//    }
+    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+    public BeerPagedList(@JsonProperty("content") List<BeerDto> content,
+                         @JsonProperty("number") int number,
+                         @JsonProperty("size") int size,
+                         @JsonProperty("totalElements") Long totalElements,
+                         @JsonProperty("pageable") JsonNode pageable,
+                         @JsonProperty("last") boolean last,
+                         @JsonProperty("totalPages") int totalPages,
+                         @JsonProperty("sort") JsonNode sort,
+                         @JsonProperty("first") boolean first,
+                         @JsonProperty("numberOfElements") int numberOfElements) {
+
+        super(content, PageRequest.of(number, size), totalElements);
+    }
 
     public BeerPagedList(List<BeerDto> content, Pageable pageable, long total) {
         super(content, pageable, total);
