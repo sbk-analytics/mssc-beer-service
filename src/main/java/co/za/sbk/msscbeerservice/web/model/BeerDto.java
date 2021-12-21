de1aca8f5e9a27be4be3a851a957fc1d47aeb091
Thato Seboko <thato.seboko@gmail.com>
2021-12-21 15:56:17 +0200

    Added bean validation

--- src/main/java/co/za/sbk/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/co/za/sbk/msscbeerservice/web/model/BeerDto.java
@@ -1,10 +1,15 @@
 package co.za.sbk.msscbeerservice.web.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -14,18 +19,33 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 public class BeerDto {
-    private UUID id;
-    private Integer version;
     
+    @Null
+    private UUID id;
+    
+    @Null
+    private Integer version;
+
+    @Null
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
+
+    @Null
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
     private OffsetDateTime lastModifiedDate;
     
+    @NotBlank
     private String beerName;
     
+    @NotNull
     private BeerStyleEnum beerStyle;
     
+    @Positive
+    @NotNull
     private Long upc;
     
+    @NotNull
+    @NotNull
     private BigDecimal price;
     
     private Integer quantityOnHand;
